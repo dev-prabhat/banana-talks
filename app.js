bf301a1d716e0ed5bf4589bbbcdf724745a62549
Prabhat <singhprabhatp.s.224@gamil.com>
2021-10-20 22:34:24 +0530

    done with logic just styling is remaining

--- app.js
+++ app.js
@@ -3,8 +3,26 @@ var inputArea = document.querySelector('textarea')
 var outputArea = document.querySelector('#output')
 
 
-btnTranslate.addEventListener("click", Eventhandler)
+//var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var serverUrl = "https://api.funtranslations.com/translate/minion.json"
+
+function getText(text) {
+    return `${serverUrl}?text=${text}`
+}
 
 function Eventhandler() {
-    outputArea.innerText = inputArea.value
+
+    var inputText = inputArea.value;
+
+    fetch(getText(inputText))
+        .then(res => res.json())
+        .then(json => {
+            var translatedText = json.contents.translated
+            outputArea.innerText = translatedText
+            //console.log(json.contents.translated)
+        })
+        .catch(err => console.log(err))
 }
+
+btnTranslate.addEventListener("click", Eventhandler)
