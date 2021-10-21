d0f12e14d076254296ccc95a4ded37c9c0257654
Prabhat <singhprabhatp.s.224@gamil.com>
2021-10-21 09:21:06 +0530

    some minor debugging and fixing

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ var inputArea = document.querySelector('textarea')
 var outputArea = document.querySelector('#output')
 
 
-//var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+// var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
 var serverUrl = "https://api.funtranslations.com/translate/minion.json"
+
 
 function getText(text) {
     return `${serverUrl}?text=${text}`
@@ -15,12 +16,13 @@ function Eventhandler() {
 
     var inputText = inputArea.value;
 
+
     fetch(getText(inputText))
         .then(res => res.json())
         .then(json => {
             var translatedText = json.contents.translated
             outputArea.innerText = translatedText
-            //console.log(json.contents.translated)
+            // console.log(json.contents.translated)
         })
         .catch(err => console.log(err))
 }
