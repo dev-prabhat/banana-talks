b8af3ac290729239dd8f41bf8cd10c4723906e91
Prabhat <singhprabhatp.s.224@gamil.com>
2022-01-09 17:52:29 +0530

    changing js code to es-6 format

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
-var btnTranslate = document.querySelector("#btn-translate");
-var inputArea = document.querySelector('textarea')
-var outputArea = document.querySelector('#output')
+const btnTranslate = document.querySelector("#btn-translate");
+const inputArea = document.querySelector('textarea')
+const outputArea = document.querySelector('#output')
 
 
 // var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
-var serverUrl = "https://api.funtranslations.com/translate/minion.json"
+const serverUrl = "https://api.funtranslations.com/translate/minion.json"
 
 
-function getText(text) {
-    return `${serverUrl}?text=${text}`
-}
+// function getText(text) {
+//     return `${serverUrl}?text=${text}`
+// }
 
-function Eventhandler() {
+const getText = text => `${serverUrl}?text=${text}`
 
-    var inputText = inputArea.value;
+const Eventhandler = () => {
+
+    const inputText = inputArea.value;
 
 
     fetch(getText(inputText))
         .then(res => res.json())
         .then(json => {
-            var translatedText = json.contents.translated
+            const translatedText = json.contents.translated
             outputArea.innerText = translatedText
-            // console.log(json.contents.translated)
+            console.log(json.contents.translated)
         })
         .catch(err => console.log(err))
 }
